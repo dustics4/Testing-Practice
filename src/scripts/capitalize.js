d705fb52d5ab73d045538824cb05b77c2058c995
Konrad <konrad.babowicz21@gmail.com>
2024-09-13 20:14:36 +0100

    Completed function, Passed all tests

--- src/scripts/capitalize.js
+++ src/scripts/capitalize.js
@@ -1,7 +1,12 @@
 const capitalize = (string) =>{
-    let str = string;
-    str = "String";;
-    return str;
+    let char = string.charAt(0);
+    let toUpperCase = char.toUpperCase();
+    let splitStr = string.substring(1);
+    
+    console.log(`${toUpperCase}` + `${splitStr}`)
+    return `${toUpperCase}` + `${splitStr}`;
 }
 
 module.exports = capitalize;
+
+capitalize("string");
