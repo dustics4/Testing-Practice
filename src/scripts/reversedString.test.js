844a1f8ac971b844a4f372418c1e9dc37a84f0b9
Konrad <konrad.babowicz21@gmail.com>
2024-09-13 20:24:37 +0100

    Written tests for reversed string

--- src/scripts/reversedString.test.js
+++ src/scripts/reversedString.test.js
@@ -1,5 +1,13 @@
 import reversed from "./reversedString.js";
 
 test("Reversed String", () => {
+    expect((reversed("reversed")).toMatch(/desrever/));
+});
 
+test("Reversed String", () => {
+    expect((reversed("john")).toMatch(/nhoj/));
+});
+
+test("Not to be a number", () => {
+    expect((reversed("reversed")).to.notBe(2));
 })
