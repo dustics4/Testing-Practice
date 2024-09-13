874afdbd7652fab6d8b4e763bece7283c89eb103
Konrad <konrad.babowicz21@gmail.com>
2024-09-13 20:06:55 +0100

    Created another test, now ready to work on function

--- src/scripts/capitalize.test.js
+++ src/scripts/capitalize.test.js
@@ -8,6 +8,10 @@ it('Exact Capitalized String' , () => {
     expect(capitalize("string")).toBe("String");
 });
 
+it('Capitalized String' , () => {
+    expect(capitalize("john")).toMatch(/John/);
+});
+
 it('Not Num' , () => {
     expect(capitalize("string")).not.toBe(1);
 });
