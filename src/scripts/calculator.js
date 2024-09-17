8a4d870196d9cdf3f18bcbbd29a82f18f1de5a66
dustics4 <konrad.babowicz21@gmail.com>
2024-09-17 19:27:25 +0100

    Creating testing file

---
+++ src/scripts/calculator.js
@@ -0,0 +1,17 @@
+const add = (a , b) => {
+
+}
+
+const subtract = (a , b) => {
+
+}
+
+const divide = (a, b) => {
+
+}
+
+const multiply = (a, b) => {
+
+}
+
+module.exports = add , subtract, divide, multiply;
