a26c7a011a3f39efbb394717a171ee1f032f771c
dustics4 <konrad.babowicz21@gmail.com>
2024-09-17 19:50:47 +0100

    Finished subtract testing and function

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -3,7 +3,7 @@ const add = (a , b) => {
 }
 
 const subtract = (a , b) => {
-
+    return a - b;
 }
 
 const divide = (a, b) => {
--- src/scripts/calculator.test.js
+++ src/scripts/calculator.test.js
@@ -7,3 +7,15 @@ test("Add (4,2) is 6", () => {
 test("Add (-4, 2) to be -2", () =>{
     expect(calculator.add(-4,2)).toBe(-2);
 })
+
+test("Subract (4, 2) to be 2", () => {
+    expect(calculator.subtract(4,2)).toBe(2);
+})
+
+test("Subtract (-4, 2 to be -6", () =>{ 
+    expect(calculator.subtract(-4,2)).toBe(-6);
+})
+
+test("Subtract is a integer", () => {
+    expect(calculator.subtract(-4,2)).not.toBe(/string/);
+})
